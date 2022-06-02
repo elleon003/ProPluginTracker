0e4ad8b4a919d50c2e79e4fd5745ad346fa4dea7
elleon003 <ngrandison@ymail.com>
2022-06-02 01:22:06 -0400

    Sortable Subscription

--- ProPluginTracker/settings.py
+++ ProPluginTracker/settings.py
@@ -138,7 +138,7 @@ PHONENUMBER_DEFAULT_FORMAT = 'NATIONAL'
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/theme/static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
