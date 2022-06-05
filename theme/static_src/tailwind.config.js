9001c569a31d5975f73dff9da1567d78847697a8
elleon003 <ngrandison@ymail.com>
2022-06-05 17:45:41 -0400

    Add header

--- theme/static_src/tailwind.config.js
+++ theme/static_src/tailwind.config.js
@@ -12,14 +12,10 @@ module.exports = {
          */
 
         /*  Templates within theme app (<tailwind_app_name>/templates), e.g. base.html. */
-        '../templates/*.html',
-        '../templates/**/*.html',
+        './templates/*.html',
+        './templates/**/*.html',
 
-        /* 
-         * Main templates directory of the project (BASE_DIR/templates).
-         * Adjust the following line to match your project structure.
-         */
-        '../../templates/**/*.html',
+       
         
         /* 
          * Templates in other django apps (BASE_DIR/<any_app_name>/templates).
@@ -46,14 +42,10 @@ module.exports = {
         extend: {},
     },
     plugins: [
-        /**
-         * '@tailwindcss/forms' is the forms plugin that provides a minimal styling
-         * for forms. If you don't like it or have own styling for forms,
-         * comment the line below to disable '@tailwindcss/forms'.
-         */
         require('@tailwindcss/forms'),
         require('@tailwindcss/typography'),
         require('@tailwindcss/line-clamp'),
         require('@tailwindcss/aspect-ratio'),
+        require("daisyui"),
     ],
 }
